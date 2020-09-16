a847ff3e62c773dbcf1bb1892a93369cc039adc8
AnonShad0w <13724916+AnonShad0w@users.noreply.github.com>
2020-09-16 14:29:28 -0700

    Update server_v2.py

--- server_v2.py
+++ server_v2.py
@@ -2,7 +2,7 @@ import socket
 import time
 
 
-HEADERSIZE = 10
+BUFFERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1243))
@@ -14,25 +14,29 @@ while True:
     print(f"Connection from {address} has been established.")
 
     from_client = ''
-    while True:
-        login_msg = clientsocket.recv(2048)
-        from_client += login_msg.decode("utf-8")
 
-        if from_client == 'password':
-            msg = "Access Granted"
-            clientsocket.send(bytes(msg,"utf-8"))
-            
-            msg = f"{len(msg):<{HEADERSIZE}}"+msg
-            clientsocket.send(bytes(msg,"utf-8"))
+    login_msg = clientsocket.recv(2048)
+    from_client += login_msg.decode("utf-8")
+
+    if from_client == 'password':
+        msg = "Access Granted"
+        clientsocket.send(bytes(msg,"utf-8"))
         
-            while True:
-                time.sleep(5)
-                msg = f"The time is {time.asctime()}"
-                msg = f"{len(msg):<{HEADERSIZE}}"+msg
+        msg = f"{len(msg):<{BUFFERSIZE}}"+msg
+        clientsocket.send(bytes(msg,"utf-8"))
+    else:
+        break
+    
+    while True:
+        time.sleep(5)
+        msg = f"The time is {time.asctime()}"
+        msg = f"{len(msg):<{BUFFERSIZE}}"+msg
 
-                print(msg)
+        print(msg)
 
-                clientsocket.send(bytes(msg,"utf-8"))
-        else:
-            break
-    break
+        clientsocket.send(bytes(msg,"utf-8"))
+
+fail_msg = "Access Denied"
+clientsocket.send(bytes(fail_msg,"utf-8"))
+clientsocket.close()
+print(f"Connection from {address} has been closed.")
